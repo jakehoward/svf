f96c997f5ceaad18eac18a8f5990a07a4eec1fab
jakehoward <j.r.g.howard@gmail.com>
2016-08-19 20:51:03 +0100

    Add user friendly error messages and more field string tests

--- field_string_parser.go
+++ field_string_parser.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
@@ -13,11 +14,62 @@ func parseFieldString(fs string) ([]int, error) {
 	}
 	fields := strings.Split(fs, ",")
 	for _, f := range fields {
-		i, err := strconv.Atoi(f)
+		is, err := parseFieldToken(f)
 		if err != nil {
-			return nil, fmt.Errorf("Fields can only be numerical values, got: %s", f)
+			return nil, err
 		}
-		fieldNums = append(fieldNums, i)
+		for _, i := range is {
+			if i == 0 {
+				return nil, errors.New("0 is not a valid field index, fields is 1 based")
+			}
+			fieldNums = append(fieldNums, i)
+		}
 	}
 	return fieldNums, nil
+}
+
+func parseFieldToken(s string) ([]int, error) {
+	var nums = []int{}
+	if len(s) == 0 {
+		return nums, nil
+	}
+
+	if !strings.Contains(s, "-") {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to parse '%s' as number in field string", s)
+		}
+		return []int{num}, nil
+	}
+
+	se := strings.Split(s, "-")
+	var err error
+	if len(se) == 2 {
+		if se[1] == "" {
+			return nil, fmt.Errorf("Field string syntax n- not yet supported. Offending section: %s-", se[0])
+		}
+
+		start, err := strconv.Atoi(se[0])
+		if err != nil {
+			return nil, fmt.Errorf("Unable to parse '%s' as number in field string", se[0])
+		}
+
+		end, err := strconv.Atoi(se[1])
+		if err != nil {
+			return nil, fmt.Errorf("Unable to parse '%s' as number in field string", se[1])
+		}
+		if start == end || start > end {
+			return nil, fmt.Errorf("Start must be smaller than end in field string range, start: %d, end: %d", start, end)
+		}
+		nums = makeRangeIncl(start, end)
+	}
+	return nums, err
+}
+
+func makeRangeIncl(s, e int) []int {
+	var nums = []int{}
+	for i := s; i <= e; i++ {
+		nums = append(nums, i)
+	}
+	return nums
 }
--- field_string_parser_test.go
+++ field_string_parser_test.go
@@ -14,13 +14,63 @@ func TestFieldStringParser(t *testing.T) {
 		{"1", []int{1}},
 		{"3,4", []int{3, 4}},
 		{"1,457,2", []int{1, 457, 2}},
+		{"1-3", []int{1, 2, 3}},
 	}
 	var actual []int
+	var err error
 	for _, test := range tests {
-		actual, _ = parseFieldString(test.fieldString)
+		if actual, err = parseFieldString(test.fieldString); err != nil {
+			t.Errorf("Expected: %v, but got error %v", test.expected, err)
+			break
+		}
 		if !reflect.DeepEqual(test.expected, actual) {
 			t.Errorf("Expected: %v, Actual: %v", test.expected, actual)
 		}
-		actual = nil
+		actual, err = nil, nil
+	}
+}
+
+func TestFieldStringRangeParser(t *testing.T) {
+	tests := []struct {
+		rangeString string
+		expected    []int
+	}{
+		{"", []int{}},
+		{"1", []int{1}},
+		{"1-3", []int{1, 2, 3}},
+		{"1-2", []int{1, 2}},
+		{"120-125", []int{120, 121, 122, 123, 124, 125}},
+	}
+	var actual []int
+	var err error
+	for _, test := range tests {
+		if actual, err = parseFieldToken(test.rangeString); err != nil {
+			t.Errorf("Expected %v, but got error %v", test.expected, err)
+			break
+		}
+		if !reflect.DeepEqual(test.expected, actual) {
+			t.Errorf("Expected: %v, Actual: %v", test.expected, actual)
+		}
+		actual, err = nil, nil
+	}
+}
+
+func TestFieldStringParserErrors(t *testing.T) {
+	tests := []struct {
+		fieldString string
+	}{
+		{"3-2"},
+		{"1-"},
+		{"5-a"},
+		{"z-5"},
+		{"1,2 ,4"},
+		{"1,0,3-4"},
+	}
+	var err error
+	for _, test := range tests {
+		if _, err = parseFieldString(test.fieldString); err == nil {
+			t.Errorf("Expected error but error was nil for input: %v", test.fieldString)
+		}
+		err = nil
 	}
 }
